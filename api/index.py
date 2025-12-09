19681b5bf2b71cc68ddea36351c12e1273b7803a
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2025-12-08 23:36:44 -0500

    fixing some issue from the debugging logs

--- api/index.py
+++ api/index.py
@@ -4,6 +4,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foreign.settings")
 
-# Expose the WSGI application for Vercel's Python runtime
+# WSGI entrypoint for Vercel (@vercel/python expects `app`)
 app = get_wsgi_application()
-handler = app
